8eef629732450011fbb15325fa05bce4c57bc0f5
Avnish Singh <avnish1708@gmail.com>
2025-06-24 23:32:46 +0530

    Dockerfile

--- src/model_training.py
+++ src/model_training.py
@@ -4,6 +4,8 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score,roc_auc_score
 from src.logger import get_logger
 from src.custom_exception import CustomException
+import dagshub
+dagshub.init(repo_owner='avnishs17', repo_name='cancer', mlflow=True)
 
 import mlflow
 import mlflow.sklearn
